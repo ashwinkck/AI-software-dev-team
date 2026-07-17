aac6fdfae7057aac7bf74f3de3d11bae5b1fcb69
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-18 03:01:41 +0530

    Refactor planner function to improve task handling and response formatting

--- ai_team/agents/planner.py
+++ ai_team/agents/planner.py
@@ -1,3 +1,30 @@
 from ai_team.state import AgentState
 from ai_team.llm import llm
 
+def planner(state: AgentState):
+
+    task = state["task"]
+
+    prompt = f"""
+You are a project planner.planner
+
+Decide whether the following task requires:
+
+research
+or
+code 
+
+Reply with only one word. 
+
+Task:
+{task}
+"""
+    
+    response = llm.invoke(prompt)
+
+    decision = response.content.strip().lower()
+
+    return {
+        "decision":decision
+    }
+
