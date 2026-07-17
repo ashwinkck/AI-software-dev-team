11fb5f0017e3abf0390615a033393a9d7ab6762b
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-17 21:41:21 +0530

    little update

--- ai_team/graph.py
+++ ai_team/graph.py
@@ -1,7 +1,15 @@
 from langgraph.graph import StateGraph, START, END
 
-from state import AgentState
-from agents.planner import planner
+from ai_team.state import AgentState
+from ai_team.agents.planner import planner
 
 
 graph_builder = StateGraph(AgentState)
+
+graph_builder.add_node("planner", planner)
+
+graph_builder.add_edge(START, "planner")
+
+graph_builder.add_edge("planner", END)
+
+graph = graph_builder.compile()
