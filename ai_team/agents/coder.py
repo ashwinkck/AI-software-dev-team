866449ea5165142d598027834568891efc00ba6d
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-18 01:41:32 +0530

    created coder and researcher

---
+++ ai_team/agents/coder.py
@@ -0,0 +1,9 @@
+from ai_team.state import AgentState
+
+def coder(state: AgentState):
+
+    print("Coding Agent Executed")
+
+    return{
+        "code": "print('Hello World')"
+    }
---
+++ ai_team/agents/researcher.py
@@ -0,0 +1,10 @@
+from ai_team.state import AgentState
+
+
+def researcher(state: AgentState):
+
+    print("Research Agent Executed")
+
+    return {
+        "research": "Research completed successfully"
+    }
